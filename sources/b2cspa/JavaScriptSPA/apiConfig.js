2043b99e8a8369b94435a61c25974a0c042d2e87
Moim HA <moimhossain@gmail.com>
2020-12-30 09:22:33 +0100

    Added a node js app

---
+++ sources/b2cspa/JavaScriptSPA/apiConfig.js
@@ -0,0 +1,5 @@
+// The current application coordinates were pre-registered in a B2C tenant.
+const apiConfig = {
+  b2cScopes: ["https://woodbineb2c.onmicrosoft.com/fd30d38f-0b11-40e6-82bb-21e901cdf03b/Demo.Read"],
+  webApi: "https://fabrikamb2chello.azurewebsites.net/hello"
+};
